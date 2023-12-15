85d43db43cf3eb6bd5d7e88edb3d5e8f8015c7d7
Ghost_burns <burab571@gmail.com>
2023-12-15 09:00:58 -0800

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char a;
-	for (a = 'a'; a <= 'z'; a++)
-		  putchar(a);
-	putchar('\n');
+char a;
+for (a = 'a'; a <= 'z'; a++)
+	putchar(a);
+putchar('\n');
 
-	return (0);
+return (0);
 }
