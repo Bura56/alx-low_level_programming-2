fa20334120e43516ff2c184584c3ed1319464820
Ghost_burns <burab571@gmail.com>
2023-12-15 11:00:19 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry block
+ * 
+ * Return: 0
+ */
+int main(void
+{
+int i = 0;
+while (i < 1)	
+{
+putchar(i % 10 + '0');
+i++;
+}
+putchar('\n');
+return (0);
+}
